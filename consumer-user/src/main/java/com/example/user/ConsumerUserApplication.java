2378c3e7682254bbe012a64f52a8584df6b0f4f9
xiaodongdong <614892011@qq.com>
2018-11-29 10:54:44 +0800

    1

--- consumer-user/src/main/java/com/example/user/ConsumerUserApplication.java
+++ consumer-user/src/main/java/com/example/user/ConsumerUserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 消费者
  * 1.引入dubbo和zookeeper相关包
  * 2.配置dubbo注册中心地址
- * 3.引用服务
+ * 3.引用服务s
  */
 @SpringBootApplication
 public class ConsumerUserApplication {
